09976251d0f02911f901f5e4a58b20a7f8f5dbfe
uttsav <uttsav.mishra.work@gmail.com>
2025-07-12 17:09:29 +0000

    Day 22 : 1 DsaQuestion

--- src/Arrays_2/FindtheDuplicateNumber.java
+++ src/Arrays_2/FindtheDuplicateNumber.java
@@ -69,6 +69,8 @@ public class FindtheDuplicateNumber {
     public static void main(String[] args) {
         int nums[] = {3,1,3,4,2};
         System.out.println(findDuplicate(nums));
+        System.out.println(findDuplicate(nums));
+
     }
 
 
